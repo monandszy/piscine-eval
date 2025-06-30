e1a5b5d3b5072e854539c5980f40120171bfbc64
monandszy <monandszy@tuta.io>
2025-06-30 17:07:39 +0200

    other evaluators

--- copy.sh
+++ copy.sh
@@ -1,1 +1,4 @@
 cp ./../monand-piscine/shell01/evaluate.sh ./shell01/evaluate.sh
+cp ./../monand-piscine/shell00/evaluate.sh ./shell00/evaluate.sh
+cp ./../monand-piscine/c00/evaluate.c ./c00/evaluate.c
+cp ./../monand-piscine/c00/compile.sh ./c00/compile.sh
